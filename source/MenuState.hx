6f224a32a8ce0bc0c70b3ab01c073a067ed4c06c
rfran <rfran@gmail.com>
2021-09-29 11:34:02 +0200

    Add Game States

--- source/MenuState.hx
+++ source/MenuState.hx
@@ -7,16 +7,29 @@ import flixel.math.FlxMath;
 import flixel.ui.FlxButton;
 
 
-class PlayState extends FlxState
+class MenuState extends FlxState
 {
 
 	var _pixie : FlxSprite;
+	var _gameNameText : FlxText;
+	var _playButton : FlxButton;
+
 	var _pixies : Array<FlxSprite> = [];
+
+	function clickPlay (){
+		flixel.FlxG.switchState (new PlayState());
+		trace ("clickPlay");
+	}
 
 	override public function create()
 	{
 		super.create();
 
+		_gameNameText = new FlxText ( 0,0, 0, "Run Pixie", 64);
+
+		_gameNameText.screenCenter();
+
+		add(_gameNameText);
 
 		_pixie = new flixel.FlxSprite();
 		_pixie.loadGraphic ( "assets/images/pixie.png");
@@ -34,11 +47,24 @@ class PlayState extends FlxState
 			add ( pc );
 			_pixies.insert ( i, pc );
 		}
+
+
+		// add menu ui
+		_playButton = new FlxButton(0,0, "Play", clickPlay);
+		_playButton.screenCenter();
+		_playButton.y += 40;
+
+		add(_playButton);
+
 	}
 
 	override public function update(elapsed:Float)
 	{
 		super.update(elapsed);
+
+		var distance = FlxMath.distanceToMouse(_gameNameText);
+
+		_pixie.x =  distance/10;
 
 		// draw pixies
 		for ( item in _pixies ){
